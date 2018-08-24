0f267effa37b7664222c5be27eb0e845ab80d993
Brett Feeley <brett.feeley217@gmail.com>
2018-08-24 08:36:50 -0600

    latest version using defaultdict

--- login_probabilities.py
+++ login_probabilities.py
@@ -1,35 +1,57 @@
 
-cd ~/Desktop
+
+
+#Data Science interview probablility question
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+'''
+logins in one csv column, days logged in in second, non-parsed input
+
+'''
 
 users = []
 logins = []
 
-'''
-logins in one csv column, days logged in in second, non-parsed input'''
 
 filename = 'logins.csv'
 
+#open and parse file
 with open(filename) as f:
     for i in f:
         users.append(i.split(',')[0])
         logins.append(i.split(',')[1])
 
-d = {}
 
+
+#create dictionary - convert to numbers while filling dictionary
+from collections import defaultdict
+d=defaultdict(list)
 for i,j in zip(users, logins):
-    d.setdefault(int(i), []).append(int(j))
+    d[int(i)].append(int(j))
     
     
+    
+#define function    
 def prob(day):
     ans=[]
     for i in d.values():
-        length = len(i)
-        if any(j>day for j in i):
+        if any(j>day for j in i) and (day in i):
             ans.append(1)
-    return (sum(ans) / len(d.values()))
+        elif day in i:
+            ans.append(0)
+    return np.mean(ans)
         
 
-prob(1)
 
-for i in range(0,8):
-    print(prob(i))
+#get probabilities for each day
+d_ans={}
+for i in range(0,7):
+   d_ans[i] = prob(i)
+
+
+#plot probs by day
+fig = plt.bar(d_ans.keys(), d_ans.values())
